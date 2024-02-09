d4bb59e0f4ecf4b7b38d2e1a5ad4d93dd2987016
shay876 <111309646+shay876@users.noreply.github.com>
2024-02-08 23:57:46 -0700

    Day 2 footer, cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import HeroSection from './components/HeroSection';
+import Home from './components/pages/Home';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
+    <>
     <Router>
+   <Navbar />
     
-    <Navbar />
-    <HeroSection />
+    <Home />
      
-     
+    <Footer />
+    
      </Router>
+     </>
   );
 }
 
