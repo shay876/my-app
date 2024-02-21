4a29e35cb67b644b7530247224d51c5c616ed5c8
shay876 <111309646+shay876@users.noreply.github.com>
2024-02-20 21:29:52 -0700

    build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ window.addEventListener('resize', showButton);
       <nav className='navbar'>
         <div className='navbar-container'>
             <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-             TRVL <i className="fab fa-typo3" />
+             UNITY TRAVEL SHOPPE <i className="fab fa-typo3" />
             </Link>
             <div className='menu-icon' onClick = {handleClick}>
               <i className= {click ? 'fas fa-times' : 'fas fa-bars'} />
